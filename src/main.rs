159c926bc48295774d92e811ed730ca7e09290dd
Tomoya Kashifuku <tnyo43@gmail.com>
2023-01-01 21:16:24 +0900

    implement get index

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, Responder, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let response_body = "Hello, World!";
+
+    HttpResponse::Ok().body(response_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
